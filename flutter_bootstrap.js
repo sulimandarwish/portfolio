da873245747d360b8a56a1c55142eb7830a9d896
Sulimandarwish <sulimandarrwitch479@gmail.com>
2026-09-09 13:41:18 +0300

    Add WhatsApp button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2432318159" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2802698418" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
